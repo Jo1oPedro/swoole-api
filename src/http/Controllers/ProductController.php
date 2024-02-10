ba7f367a6be1dd720ffda87d7298b4a7438369bb
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-10 19:50:57 -0300

    adiciona metodo show e callback function em uma rota de teste

--- src/http/Controllers/ProductController.php
+++ src/http/Controllers/ProductController.php
@@ -2,17 +2,22 @@
 
 namespace App\http\Controllers;
 
+use App\Database\Connection;
 use Cascata\Framework\Http\Response;
 
 class ProductController
 {
-    public function index()
+    public function index(): Response
     {
         return Response::ok('sucesso');
     }
 
-    public function show(int $id)
+    public function show(int $id): Response
     {
+        $user = Connection::getInstance()
+            ->query('Select * from users')
+            ->fetchAll();
 
+        return Response::ok($user);
     }
 }
--- src/http/web/routes.php
+++ src/http/web/routes.php
@@ -6,4 +6,7 @@ use Cascata\Framework\Http\route\RouterGrouper;
 $router = RouterGrouper::getInstance();
 
 $router->addRoute('GET', '/products', [ProductController::class, 'index']);
-$router->addRoute('GET', '/product/{id:\d+}', [ProductController::class, 'show']);
+$router->addRoute('GET', '/products/{id:\d+}', [ProductController::class, 'show']);
+$router->addRoute('GET', '/x', function (\Swoole\Http\Request $request) {
+    return \Cascata\Framework\Http\Response::ok('oi mundo');
+});
