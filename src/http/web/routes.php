36303ea9620abade44a73af2e462b62c08d4d483
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-12 10:44:55 -0300

    altera arquivo de rotas e adiciona middleware group

--- src/http/web/routes.php
+++ src/http/web/routes.php
@@ -1,14 +1,21 @@
 <?php
 
 use App\http\Controllers\ProductController;
-use Cascata\Framework\Http\route\RouterGrouper;
+use Cascata\Framework\Http\Middleware\Authenticate;
+use Cascata\Framework\Http\Middleware\Success;
+use Cascata\Framework\Http\route\Route;
 
-$router = RouterGrouper::getInstance();
+$router = Route::getInstance();
 
 $router->addGroup('/products', function (FastRoute\RouteCollector $routes) {
     $routes->addRoute('GET', '', [ProductController::class, 'index']);
     $routes->addRoute('GET', '/{id:\d+}', [ProductController::class, 'show']);
 });
-$router->addRoute('GET', '/x', function (\Swoole\Http\Request $request) {
+
+$router->addMiddlewareGroup(function (Route $route) {
+    $route->addRoute('GET', '/cascata', [ProductController::class, 'index']);
+}, [Authenticate::class]);
+
+$router->addRoute('GET', '/x', function (\Swoole\Http\Request $request, ProductController $productController) {
     return \Cascata\Framework\Http\Response::ok('oi mundo');
 });
