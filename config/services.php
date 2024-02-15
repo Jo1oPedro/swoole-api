70e3aa1a0adade2a9faa6f97886b1603d6fdbf91
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-15 00:06:43 -0300

    adiciona conexão ao memcached no container

--- config/services.php
+++ config/services.php
@@ -8,18 +8,27 @@ use Monolog\Logger;
 
 $container = GlobalContainer::getInstance();
 
+## LOGGER
 $container->set('logger', function () {
     $logger = new Logger('app');
     $logger->pushHandler(new StreamHandler(BASE_PATH . "/" . $_ENV['LOG_STORAGE']));
     return $logger;
 });
 
+## ROUTE
 $container->set(Route::class, function () {
     return Route::getInstance();
 });
 
+
+## CONEXAO MYSQL
 $container->set(PDO::class, function () {
     return Connection::getInstance();
 });
+
+## CACHE
+$memcached = new Memcached();
+$memcached->addServer('banco_de_dados_em_memoria', 11211);
+$container->set(Memcached::class, $memcached);
 
 return $container;
