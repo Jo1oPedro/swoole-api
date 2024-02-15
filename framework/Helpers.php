df2b4616dcc2d802dcc7031424207831d29f7a63
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-15 00:04:00 -0300

    adiciona arquivo de funções auxiliares

---
+++ framework/Helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Swoole\Http\Request;
+
+function getAuthorizationToken(Request $request): string
+{
+    return str_replace('Bearer ', '', $request->header['authorization']);
+}
+
+function getAuthenticatedUserData(Request $request): stdClass
+{
+    return JWT::decode(
+        getAuthorizationToken($request),
+        new Key(
+            $_ENV['JWT_KEY'],
+            'HS256'
+        )
+    );
+}
