1d578433378a4122b4d26ead3cf2ba0be75df9f6
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-15 00:08:32 -0300

    adiciona singOutController com método para deslogar

---
+++ src/http/Controllers/SignOutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\http\Controllers;
+
+use Cascata\Framework\Http\Response;
+use Swoole\Http\Request;
+
+class SignOutController
+{
+    public function __construct(
+        private \Memcached $memcached
+    ) {}
+
+    public function signOut(Request $request)
+    {
+        $token = getAuthorizationToken($request);
+        $bool = $this->memcached->set("{$token}", $token, time() + 60 * 60);
+        return Response::ok(json_encode((int) $bool));
+    }
+}
