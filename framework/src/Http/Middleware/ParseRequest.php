987b626b2ce4d24423731a83a0156550b3625605
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-15 00:09:13 -0300

    adiciona validação se cabeçalho de content-type foi enviado

--- framework/src/Http/Middleware/ParseRequest.php
+++ framework/src/Http/Middleware/ParseRequest.php
@@ -9,7 +9,11 @@ class ParseRequest implements MiddlewareInterface
 {
     public function process(Request $request, RequestHandlerInterface $next): Response
     {
-        if($request->getMethod() === 'POST' && $request->header['content-type'] === 'application/json') {
+        if(
+            $request->getMethod() === 'POST' &&
+            (isset($request->header['content-type']) &&
+            $request->header['content-type'] === 'application/json')
+        ) {
             $request->post = json_decode($request->rawContent(), true);
         }
         return $next->handle($request);
