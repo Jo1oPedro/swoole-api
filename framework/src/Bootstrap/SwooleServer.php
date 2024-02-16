e0c33a62924ae34dad40bbd74cb245262f1f1a55
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-16 17:30:23 -0300

    adiciona classe de bootstrap do servidor swoole

---
+++ framework/src/Bootstrap/SwooleServer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Cascata\Framework\Bootstrap;
+
+use Cascata\Framework\Http\Middleware\RequestHandler;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Swoole\Http\Server;
+
+class SwooleServer
+{
+    private static Server $server;
+
+    public static function start()
+    {
+        self::$server = new Server('0.0.0.0', (int) $_ENV['PORT']);
+        self::onStart();
+        self::onRequest();
+        self::$server->start();
+    }
+
+    private static function onStart()
+    {
+        self::$server->on('start', function (Server $server) {
+            echo 'HTTP Server ready at http://localhost:' . $_ENV['PORT'] . PHP_EOL;
+        });
+    }
+
+    private static function onRequest()
+    {
+        self::$server->on('request', function (Request $request, Response $response) {
+            if($request->server['request_uri'] === '/favicon.ico') {
+                $response->end('');
+                return;
+            };
+
+            $requestWrapper = new \Cascata\Framework\Http\Request($request);
+            $requestHandler = new RequestHandler();
+
+            list(
+                $statusCode,
+                $headers,
+                $content
+                ) = $requestHandler->handle($requestWrapper)->toArray();
+
+            $response->setStatusCode($statusCode ?? 200);
+            foreach($headers as $header => $value) {
+                $response->setHeader($header, $value);
+            }
+            $response->end($content);
+
+            /*if(isset($request->server['query_string'])) {
+                parse_str($request->server['query_string'], $params);
+            }*/
+        });
+    }
+}
--- public/server.php
+++ public/server.php
@@ -1,10 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 use Cascata\Framework\Bootstrap\Dependencies;
-use Cascata\Framework\Http\Middleware\RequestHandler;
-use Swoole\Http\Request;
-use Swoole\Http\Response;
-use Swoole\Http\Server;
+use Cascata\Framework\Bootstrap\SwooleServer;
 
 Co::set(['hook_flags', SWOOLE_HOOK_ALL]);
 
@@ -14,37 +13,4 @@ require_once BASE_PATH . "/vendor/autoload.php";
 require_once BASE_PATH . "/src/routes/api.php";
 
 Dependencies::start();
-
-$server = new Server('0.0.0.0', (int) $_ENV['PORT']);
-
-$server->on('start', function (Server $server) {
-    echo 'HTTP Server ready at http://localhost:' . $_ENV['PORT'] . PHP_EOL;
-});
-
-$server->on('request', function (Request $request, Response $response) {
-    if($request->server['request_uri'] === '/favicon.ico') {
-       $response->end('');
-       return;
-    };
-
-    $requestWrapper = new \Cascata\Framework\Http\Request($request);
-    $requestHandler = new RequestHandler();
-
-    list(
-        $statusCode,
-        $headers,
-        $content
-    ) = $requestHandler->handle($requestWrapper)->toArray();
-
-    $response->setStatusCode($statusCode ?? 200);
-    foreach($headers as $header => $value) {
-        $response->setHeader($header, $value);
-    }
-    $response->end($content);
-
-    /*if(isset($request->server['query_string'])) {
-        parse_str($request->server['query_string'], $params);
-    }*/
-});
-
-$server->start();
+SwooleServer::start();
