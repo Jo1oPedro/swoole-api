891fa7fb80612560a12f6e5e3ce4906606207181
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-02-20 17:25:49 -0300

    corrige bootstrap do phpunit

--- bootstrap-phpunit.php
+++ bootstrap-phpunit.php
@@ -1,10 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+use Cascata\Framework\Bootstrap\Dependencies;
+use Cascata\Framework\Bootstrap\Events;
 use Symfony\Component\Dotenv\Dotenv;
 
 include_once __DIR__ . '/vendor/autoload.php';
 
-const ROOT_DIR = __DIR__;
+Co::set(['hook_flags', SWOOLE_HOOK_ALL]);
+
+define("BASE_PATH", __DIR__);
 
 $dotenv = new Dotenv();
-$dotenv->load();
+$dotenv->load(BASE_PATH . "/tests/.env");
+
+require_once BASE_PATH . "/vendor/autoload.php";
+require_once BASE_PATH . "/src/routes/api.php";
+
+Dependencies::start();
+Events::registerEvents();
